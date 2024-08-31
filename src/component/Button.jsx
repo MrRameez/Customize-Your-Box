89a4e50fca74762c6bcbfdd319bb5a8df175224c
Rameez329732 <156177291+Rameez329732@users.noreply.github.com>
2024-08-31 18:57:13 +0500

    first commit

---
+++ src/component/Button.jsx
@@ -0,0 +1,18 @@
+function Button({content,bgcolor,fontcolor,onclick,childButton}){
+    return(
+        <button 
+        className='bg-slate-200 bord rounded-lg ml-5 my-2'
+        onClick={onclick}
+        style={{
+            backgroundColor:bgcolor?bgcolor:"skyblue",
+            color:fontcolor?fontcolor:"black",
+            margin:"10px",
+            padding:"1px",
+            width: "75px",
+            height: "30px",
+            borderRadius: "5px",
+        }}>{content}</button>
+    )
+}
+
+export default Button
